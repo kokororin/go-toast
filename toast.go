b81b33e84fe28d7d550a7059993019e74c851a8a
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-08-15 10:37:42 -0400

    Pass in script via stdin

--- toast.go
+++ toast.go
@@ -345,7 +345,8 @@ func Duration(name string) (toastDuration, error) {
 func invokeTemporaryScript(content string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	cmd := exec.CommandContext(ctx, "PowerShell", "-NoProfile", "-NonInteractive", content)
+	cmd := exec.CommandContext(ctx, "PowerShell", "-NoProfile", "-NonInteractive", "-")
+	cmd.Stdin = strings.NewReader(content)
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	if err := cmd.Run(); err != nil {
 		return err
